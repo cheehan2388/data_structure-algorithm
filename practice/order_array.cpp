dc99467de93a2b3f74507814ee56ec643ed4dd76
cheehan2388 <cheehan2388@gmail.com>
2026-06-12 15:53:09 +0800

    mark ordercpp

--- practice/order_array.cpp
+++ practice/order_array.cpp
@@ -2,9 +2,10 @@
 #include <stdbool.h>
 
 
-void order_array(int b[],int* len,int a)
+void order_array(int b[],int* len,int a)//用int* 是希望他直接該全局的（指向該地址，而不是複製）
+//int b[] 本身就是指地址，如果之後不要更改裏面的就加一個 const
  {
-   int i = *len - 1 ;
+   int i = *len - 1 ; //*len 前面定義了 int* len ，所以*len 是取出地址的值
    while(i >= 0 && b[i]>a) {
         b[i+1] = b[i] ;
         i -- ;     
